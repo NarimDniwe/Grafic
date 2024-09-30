caf9722f2935e932a010c1a02bc5f230c0656f77
nickKosenko <nick.kosenko2002@gmail.com>
2024-09-30 20:22:46 +0300

    Added JFrame and CanvasPanel

--- src/second_version/Main.java
+++ src/second_version/Main.java
@@ -1,7 +1,28 @@
 package second_version;
 
+import java.awt.Graphics;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class Main {
     public static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+
+        CanvasPanel panel = new CanvasPanel();
+        frame.add(panel);
+
+    }
+}
+
+class CanvasPanel extends JPanel {
+
+    @Override
+    protected void paintComponent(Graphics g) {
 
     }
 }
